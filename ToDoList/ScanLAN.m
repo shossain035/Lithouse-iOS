745ec088e0652dcd88a1a896f5ca26a885d64ce5
shossain035 <shah@mac-mini.gateway.2wire.net>
2014-04-06 18:12:20 -0700

    ipscan

---
+++ ToDoList/ScanLAN.m
@@ -0,0 +1,329 @@
+//
+//  ScanLAN.m
+//  LAN Scan
+//
+//  Created by Mongi Zaidi on 24 February 2014.
+//  Copyright (c) 2014 Smart Touch. All rights reserved.
+//
+
+#import "ScanLAN.h"
+#import <ifaddrs.h>
+#import <arpa/inet.h>
+#include <netdb.h>
+#import "SimplePingHelper.h"
+#include <sys/param.h>
+#include <sys/file.h>
+#include <sys/socket.h>
+#include <sys/sysctl.h>
+
+#include <net/if.h>
+#include <net/if_dl.h>
+#include <net/ethernet.h>
+//#include <net
+//#include "route.h"
+//#include "if_ether.h"
+#include <netinet/in.h>
+
+
+#include <arpa/inet.h>
+
+#include <err.h>
+#include <errno.h>
+#include <netdb.h>
+
+#include <paths.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define RTF_LLINFO	0x400
+
+struct rt_metrics {
+	u_int32_t	rmx_locks;	/* Kernel must leave these values alone */
+	u_int32_t	rmx_mtu;	/* MTU for this path */
+	u_int32_t	rmx_hopcount;	/* max hops expected */
+	int32_t		rmx_expire;	/* lifetime for route, e.g. redirect */
+	u_int32_t	rmx_recvpipe;	/* inbound delay-bandwidth product */
+	u_int32_t	rmx_sendpipe;	/* outbound delay-bandwidth product */
+	u_int32_t	rmx_ssthresh;	/* outbound gateway buffer limit */
+	u_int32_t	rmx_rtt;	/* estimated round trip time */
+	u_int32_t	rmx_rttvar;	/* estimated rtt variance */
+	u_int32_t	rmx_pksent;	/* packets sent using this route */
+	u_int32_t	rmx_filler[4];	/* will be used for T/TCP later */
+};
+
+struct rt_msghdr {
+	u_short	rtm_msglen;		/* to skip over non-understood messages */
+	u_char	rtm_version;		/* future binary compatibility */
+	u_char	rtm_type;		/* message type */
+	u_short	rtm_index;		/* index for associated ifp */
+	int	rtm_flags;		/* flags, incl. kern & message, e.g. DONE */
+	int	rtm_addrs;		/* bitmask identifying sockaddrs in msg */
+	pid_t	rtm_pid;		/* identify sender */
+	int	rtm_seq;		/* for sender to identify action */
+	int	rtm_errno;		/* why failed */
+	int	rtm_use;		/* from rtentry */
+	u_int32_t rtm_inits;		/* which metrics we are initializing */
+	struct rt_metrics rtm_rmx;	/* metrics themselves */
+};
+
+struct sockaddr_inarp {
+    u_char  sin_len;
+    u_char  sin_family;
+    u_short sin_port;
+    struct  in_addr sin_addr;
+    struct  in_addr sin_srcaddr;
+    u_short sin_tos;
+    u_short sin_other;
+#define SIN_PROXY 1
+};
+
+
+@interface ScanLAN ()
+
+@property NSString *localAddress;
+@property NSString *baseAddress;
+@property NSInteger currentHostAddress;
+@property NSTimer *timer;
+@property NSString *netMask;
+@property NSInteger baseAddressEnd;
+@property NSInteger timerIterationNumber;
+
+@end
+
+@implementation ScanLAN
+
+- (id)initWithDelegate:(id<ScanLANDelegate>)delegate {
+    NSLog(@"init scanner");
+    self = [super init];
+    if(self)
+    {
+		self.delegate = delegate;
+    }
+    return self;
+}
+
+- (void)startScan {
+    NSLog(@"start scan");
+    self.localAddress = [self localIPAddress];
+    //This is used to test on the simulator
+    //self.localAddress = @"192.168.1.8";
+    //self.netMask = @"255.255.255.0";
+    NSArray *a = [self.localAddress componentsSeparatedByString:@"."];
+    NSArray *b = [self.netMask componentsSeparatedByString:@"."];
+    if ([self isIpAddressValid:self.localAddress] && (a.count == 4) && (b.count == 4)) {
+        for (int i = 0; i<4; i++) {
+            int and = [[a objectAtIndex:i] integerValue] & [[b objectAtIndex:i] integerValue];
+            if (!self.baseAddress.length) {
+                self.baseAddress = [NSString stringWithFormat:@"%d", and];
+            }
+            else {
+                self.baseAddress = [NSString stringWithFormat:@"%@.%d", self.baseAddress, and];
+                self.currentHostAddress = and;
+                self.baseAddressEnd = and;
+            }
+        }
+        self.timer = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(pingAddress) userInfo:nil repeats:YES];
+    }
+}
+
+- (void)stopScan {
+    NSLog(@"stop scan");
+    [self.timer invalidate];
+}
+
+- (void)pingAddress{
+    self.currentHostAddress++;
+    NSString *address = [NSString stringWithFormat:@"%@%d", self.baseAddress, self.currentHostAddress];
+    [SimplePingHelper ping:address target:self sel:@selector(pingResult:)];
+    if (self.currentHostAddress>=254) {
+        [self.timer invalidate];
+    }
+}
+/*
+ - (void)pingAddress:(NSString *)address{
+ [SimplePingHelper ping:address target:self sel:@selector(pingResult:)];
+ }
+ */
+- (void)pingResult:(NSNumber*)success {
+    self.timerIterationNumber++;
+    if (success.boolValue) {
+        NSLog(@"SUCCESS");
+        NSString *deviceIPAddress = [[[[NSString stringWithFormat:@"%@%d", self.baseAddress, self.currentHostAddress] stringByReplacingOccurrencesOfString:@".0" withString:@"."] stringByReplacingOccurrencesOfString:@".00" withString:@"."] stringByReplacingOccurrencesOfString:@".." withString:@".0."];
+        NSLog(@"MAC = %@", [self ip2mac:inet_addr([ deviceIPAddress UTF8String ])]);
+        NSString *deviceName = [self getHostFromIPAddress:[[NSString stringWithFormat:@"%@%d", self.baseAddress, self.currentHostAddress] cStringUsingEncoding:NSASCIIStringEncoding]];
+        [self.delegate scanLANDidFindNewAdrress:deviceIPAddress havingHostName:deviceName];
+    }
+    else {
+        NSLog(@"FAILURE");
+    }
+    if (self.timerIterationNumber+self.baseAddressEnd>=254) {
+        [self.delegate scanLANDidFinishScanning];
+    }
+}
+
+- (NSString *)getHostFromIPAddress:(const char*)ipAddress {
+    NSString *hostName = nil;
+    int error;
+    struct addrinfo *results = NULL;
+    
+    error = getaddrinfo(ipAddress, NULL, NULL, &results);
+    if (error != 0)
+    {
+        NSLog (@"Could not get any info for the address");
+        return nil; // or exit(1);
+    }
+    
+    for (struct addrinfo *r = results; r; r = r->ai_next)
+    {
+        char hostname[NI_MAXHOST] = {0};
+        error = getnameinfo(r->ai_addr, r->ai_addrlen, hostname, sizeof hostname, NULL, 0 , 0);
+        if (error != 0)
+        {
+            continue; // try next one
+        }
+        else
+        {
+            NSLog (@"Found hostname: %s", hostname);
+            hostName = [NSString stringWithFormat:@"%s", hostname];
+            break;
+        }
+        
+    }
+    
+    freeaddrinfo(results);
+    return hostName;
+}
+
+- (NSString*)ip2mac:(in_addr_t)addr
+{
+    NSString *ret = nil;
+    
+    size_t needed;
+    char *buf, *next;
+    
+    struct rt_msghdr *rtm;
+    struct sockaddr_inarp *sin;
+    struct sockaddr_dl *sdl;
+    
+    int mib[6];
+    
+    mib[0] = CTL_NET;
+    mib[1] = PF_ROUTE;
+    mib[2] = 0;
+    mib[3] = AF_INET;
+    mib[4] = NET_RT_FLAGS;
+    mib[5] = RTF_LLINFO;
+    
+    if (sysctl(mib, sizeof(mib) / sizeof(mib[0]), NULL, &needed, NULL, 0) < 0)
+        err(1, "route-sysctl-estimate");
+    
+    if ((buf = (char*)malloc(needed)) == NULL)
+        err(1, "malloc");
+    
+    if (sysctl(mib, sizeof(mib) / sizeof(mib[0]), buf, &needed, NULL, 0) < 0)
+        err(1, "retrieval of routing table");
+    
+    for (next = buf; next < buf + needed; next += rtm->rtm_msglen) {
+        
+        rtm = (struct rt_msghdr *)next;
+        sin = (struct sockaddr_inarp *)(rtm + 1);
+        sdl = (struct sockaddr_dl *)(sin + 1);
+        
+        if (addr != sin->sin_addr.s_addr || sdl->sdl_alen < 6)
+            continue;
+        
+        u_char *cp = (u_char*)LLADDR(sdl);
+        
+        ret = [NSString stringWithFormat:@"%02X:%02X:%02X:%02X:%02X:%02X",
+               cp[0], cp[1], cp[2], cp[3], cp[4], cp[5]];
+        
+        break;
+    }
+    
+    free(buf);
+    
+    return ret;
+}
+
+// Get IP Address
+- (NSString *)getIPAddress
+{
+    struct ifaddrs *interfaces = NULL;
+    struct ifaddrs *temp_addr = NULL;
+    NSString *wifiAddress = nil;
+    NSString *cellAddress = nil;
+    
+    // retrieve the current interfaces - returns 0 on success
+    if(!getifaddrs(&interfaces)) {
+        // Loop through linked list of interfaces
+        temp_addr = interfaces;
+        while(temp_addr != NULL) {
+            sa_family_t sa_type = temp_addr->ifa_addr->sa_family;
+            if(sa_type == AF_INET || sa_type == AF_INET6) {
+                NSString *name = [NSString stringWithUTF8String:temp_addr->ifa_name];
+                NSString *addr = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)]; // pdp_ip0
+                //NSLog(@"NAME: \"%@\" addr: %@", name, addr); // see for yourself
+                
+                if([name isEqualToString:@"en0"]) {
+                    // Interface is the wifi connection on the iPhone
+                    wifiAddress = addr;
+                } else
+                    if([name isEqualToString:@"pdp_ip0"]) {
+                        // Interface is the cell connection on the iPhone
+                        cellAddress = addr;
+                    }
+            }
+            temp_addr = temp_addr->ifa_next;
+        }
+        // Free memory
+        freeifaddrs(interfaces);
+    }
+    NSString *addr = wifiAddress ? wifiAddress : cellAddress;
+    return addr ? addr : @"0.0.0.0";
+}
+
+- (NSString *) localIPAddress
+{
+    NSString *address = @"error";
+    struct ifaddrs *interfaces = NULL;
+    struct ifaddrs *temp_addr = NULL;
+    int success = 0;
+    
+    // retrieve the current interfaces - returns 0 on success
+    success = getifaddrs(&interfaces);
+    
+    if (success == 0)
+    {
+        temp_addr = interfaces;
+        
+        while(temp_addr != NULL)
+        {
+            // check if interface is en0 which is the wifi connection on the iPhone
+            if(temp_addr->ifa_addr->sa_family == AF_INET)
+            {
+                if([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"])
+                {
+                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
+                    self.netMask = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_netmask)->sin_addr)];
+                }
+            }
+            
+            temp_addr = temp_addr->ifa_next;
+        }
+    }
+    
+    freeifaddrs(interfaces);
+    
+    return address;
+}
+
+- (BOOL) isIpAddressValid:(NSString *)ipAddress{
+    struct in_addr pin;
+    int success = inet_aton([ipAddress UTF8String],&pin);
+    if (success == 1) return TRUE;
+    return FALSE;
+}
+
+@end
