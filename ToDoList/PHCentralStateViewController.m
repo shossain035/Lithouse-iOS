5743525e00ac4c1894f19c4dba5a634b9762b3be
shossain035 <shah@mac-mini.gateway.2wire.net>
2014-04-26 02:11:29 -0700

    place holder sdk integration

--- ToDoList/PHCentralStateViewController.m
+++ ToDoList/PHCentralStateViewController.m
@@ -7,10 +7,13 @@
 //
 
 #import "PHCentralStateViewController.h"
+#import <HueSDK_iOS/HueSDK.h>
 
 @interface PHCentralStateViewController ()
 
-@property UIBarButtonItem * activityIndicatorButton;
+@property (strong, nonatomic) PHHueSDK          * phHueSDK;
+@property (nonatomic, strong) PHBridgeSearching * bridgeSearch;
+@property UIBarButtonItem                       * activityIndicatorButton;
 
 @end
 
@@ -36,6 +39,37 @@
     self.navigationItem.rightBarButtonItem = self.activityIndicatorButton;
     self.navigationItem.title = @"Searching...";
     [self.navigationController setToolbarHidden : YES];
+    
+    // Create sdk instance
+    self.phHueSDK = [[PHHueSDK alloc] init];
+    [self.phHueSDK startUpSDK];
+    [self.phHueSDK enableLogging : YES];
+    
+    // Listen for notifications
+    PHNotificationManager *notificationManager = [PHNotificationManager defaultManager];
+    /***************************************************
+     The SDK will send the following notifications in response to events
+     *****************************************************/
+    
+    [notificationManager registerObject : self
+                           withSelector : @selector ( localConnection )
+                        forNotification : LOCAL_CONNECTION_NOTIFICATION];
+    [notificationManager registerObject : self
+                           withSelector : @selector ( noLocalConnection )
+                        forNotification : NO_LOCAL_CONNECTION_NOTIFICATION];
+    /***************************************************
+     If there is no authentication against the bridge this notification is sent
+     *****************************************************/
+    
+    [notificationManager registerObject : self
+                           withSelector : @selector ( notAuthenticated )
+                        forNotification : NO_LOCAL_AUTHENTICATION_NOTIFICATION];
+    /***************************************************
+     The local heartbeat is a regular  timer event in the SDK. Once enabled the SDK regular collects the current state of resources managed
+     by the bridge into the Bridge Resources Cache
+     *****************************************************/
+    
+    [self enableLocalHeartbeat];
 }
 
 - (void)didReceiveMemoryWarning
@@ -43,6 +77,79 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - Heartbeat control
+
+/**
+ Starts the local heartbeat with a 10 second interval
+ */
+- (void)enableLocalHeartbeat {
+    /***************************************************
+     The heartbeat processing collects data from the bridge
+     so now try to see if we have a bridge already connected
+     *****************************************************/
+    
+    PHBridgeResourcesCache *cache = [PHBridgeResourcesReader readBridgeResourcesCache];
+    if ( cache != nil && cache.bridgeConfiguration != nil
+        && cache.bridgeConfiguration.ipaddress != nil ) {
+        //
+        self.navigationItem.title = @"Connecting...";
+        
+        // Enable heartbeat with interval of 10 seconds
+        [self.phHueSDK enableLocalConnectionUsingInterval:10];
+    } else {
+        // Automaticly start searching for bridges
+        [self searchForBridgeLocal];
+    }
+}
+
+/**
+ Stops the local heartbeat
+ */
+- (void)disableLocalHeartbeat {
+    [self.phHueSDK disableLocalConnection];
+}
+
+#pragma mark - Bridge searching and selection
+
+/**
+ Search for bridges using UPnP and portal discovery, shows results to user or gives error when none found.
+ */
+- (void)searchForBridgeLocal {
+    // Stop heartbeats
+    [self disableLocalHeartbeat];
+    
+    self.navigationItem.title = @"Searching...";
+    /***************************************************
+     A bridge search is started using UPnP to find local bridges
+     *****************************************************/
+    
+    // Start search
+    self.bridgeSearch = [[PHBridgeSearching alloc] initWithUpnpSearch : YES
+                                                      andPortalSearch : YES
+                                                    andIpAdressSearch : YES];
+    
+    [self.bridgeSearch startSearchWithCompletionHandler : ^( NSDictionary *bridgesFound ) {
+        
+        /***************************************************
+         The search is complete, check whether we found a bridge
+         *****************************************************/
+        
+        // Check for results
+        if (bridgesFound.count > 0) {
+            NSLog ( @"bridges: %@", bridgesFound );
+        }
+        else {
+
+//            UIAlertView * alertView = [[UIAlertView alloc] initWithTitle : @"No bridge found"
+//                                                                 message : @"Could not find a Hue bridge. Please make sure that the bridge is powered up and connected to router."
+//                                                       cancelButtonTitle : @"Cancel"
+//                                                       otherButtonTitles : NSLocalizedString(@"Retry", @"No bridge found alert retry button"),NSLocalizedString(@"Cancel", @"No bridge found alert cancel button"), nil];
+//            [alertView show];
+        }
+    }];
+}
+
 
 /*
 #pragma mark - Navigation
